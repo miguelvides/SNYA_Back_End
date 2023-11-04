dfbdfec7d42de3c356bf065a062084b1f65994dc
miguelvides90 <51522845+miguelvides@users.noreply.github.com>
2023-11-03 20:33:02 -0600

    commit con cambios

--- src/main/java/com/gestion/SNYA/controlador/InstitucionControlador.java
+++ src/main/java/com/gestion/SNYA/controlador/InstitucionControlador.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +15,7 @@ import com.gestion.SNYA.repositorio.InstitucionRepositorio;
 
 @RestController
 @RequestMapping("/api/v1/")
-@CrossOrigin(origins ="http://localhost:4200")
+@CrossOrigin(origins ="http://localhost:4200/")
 
 public class InstitucionControlador {
 	@Autowired
@@ -23,5 +25,12 @@ public class InstitucionControlador {
 	public List<Institucion> listarTodasInstituciones() {
 		return repositorio.findAll();
 	}
+	
+	
+	@PostMapping("/instituciones")
+	public Institucion guardarInstitucion(@RequestBody Institucion institucion) {
+		
+	return repositorio.save(institucion);
 
+}
 }
