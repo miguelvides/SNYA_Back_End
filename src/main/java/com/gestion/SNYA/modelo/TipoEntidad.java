e7b220bef00f4b6858f91608fce5c99579806267
BrandonG1184 <GM21057@ues.edu.sv>
2023-11-05 00:52:10 -0600

    feat: se agrego la clase Enum usado para delimitar el tipo de entidad es el donante

---
+++ src/main/java/com/gestion/SNYA/modelo/TipoEntidad.java
@@ -0,0 +1,9 @@
+package com.gestion.SNYA.modelo;
+
+//Enum usado para delimitar el tipo de entidad es el donante
+public enum TipoEntidad {
+    PERSONA_NATURAL,
+    PERSONA_JURIDICA,
+    EMPRESA,
+    ORGANIZACION_SIN_ANIMOS_DE_LUCRO;
+}
