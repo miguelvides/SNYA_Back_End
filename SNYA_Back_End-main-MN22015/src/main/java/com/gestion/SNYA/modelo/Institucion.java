0dfdaac914bd9bd9fe62e25790efd47627bd4e04
osmn10 <149851225+osmn10@users.noreply.github.com>
2023-11-19 21:08:59 -0600

    Add files via upload
    Eliminar Institucion

---
+++ SNYA_Back_End-main-MN22015/src/main/java/com/gestion/SNYA/modelo/Institucion.java
@@ -0,0 +1,35 @@
+package com.gestion.SNYA.modelo;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@ToString
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Institucion {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	Integer idInstitucion;
+	String nombre;
+	String tipo_institucion;
+	Double presupuesto_institucion;
+	Date fecha_ingreso;
+	
+
+	
+	
+	
+
+
+}
---
+++ SNYA_Back_End-main-MN22015/src/main/java/com/gestion/SNYA/modelo/Presupuesto.java
@@ -0,0 +1,31 @@
+package com.gestion.SNYA.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+
+public class Presupuesto {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	Integer IdPresupuesto;
+	Integer cantidad;
+	String institucion;
+	String observacion; 
+	
+	
+	
+
+}
